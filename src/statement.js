3d66b3cb4412d77bcaafe209ab26f88ca837399d
RelativeBinary <patrick.bornay@gmail.com>
2022-04-10 14:33:05 +1000

    missed 'format' implementation refactoring, extracting /100 behaviour

--- src/statement.js
+++ src/statement.js
@@ -1,20 +1,19 @@
 function statement (invoice, plays) {
     let totalAmount = 0;
     let result = `Statement for ${invoice.customer} \n`;
-
     for (let perf of invoice.performances) {
      
         //print line for this order
-        result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf)
     }
     let volumeCredits = 0;
-    for (let perf of invoive.performances) {
+    for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
-    return result
+    return result;
 }
 
 function amountFor(aPerformance, play) {
@@ -57,6 +56,6 @@ function volumeCreditsFor(aPerformance) {
 function usd(aNumber) {
     return new Intl.NumberFormat("en-US", 
     {style: "currency", currency: "USD"})
-    .format(aNumber);
+    .format(aNumber/100);
 }
 module.exports = statement;
