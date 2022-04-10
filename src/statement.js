07b226211dc3f686e5a11ab5133efaeaebcbef24
RelativeBinary <patrick.bornay@gmail.com>
2022-04-10 14:15:59 +1000

    change function declaration to something more meaningful

--- src/statement.js
+++ src/statement.js
@@ -7,10 +7,10 @@ function statement (invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
      
         //print line for this order
-        result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf)
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result
 }
@@ -52,7 +52,7 @@ function volumeCreditsFor(aPerformance) {
     return result;
 }
 
-function format(aNumber) {
+function usd(aNumber) {
     return new Intl.NumberFormat("en-US", 
     {style: "currency", currency: "USD"})
     .format(aNumber);
